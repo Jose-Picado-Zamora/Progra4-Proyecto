6bae3f2a89026d945f0f090dfd99af16bec57b4a
Jose-Picado-Zamora <joseandrespicadozamora9@gmail.com>
2025-05-08 13:58:14 -0600

    Prueba

--- src/pages/Donadores.tsx
+++ src/pages/Donadores.tsx
@@ -2,7 +2,7 @@ const Donadores = () => {
     return (
         <div className="page-content main-container">
           <h1>Gestión de Donadores</h1>
-          <p>Aquí podrás visualizar y agregar información sobre Donadores potenciales.</p>
+          <p>Aquí podrás visualizar y agregar información sobre Donadores.</p>
         </div>
       );      
   };
