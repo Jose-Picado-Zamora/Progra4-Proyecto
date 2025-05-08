7d1e1f21bc9e722696eeebf47fbf0471d9eb2ddd
Brandon-Corrales <brandon.nunez.corrales@est.una.ac.cr>
2025-05-07 21:15:51 -0600

    Implementación de Navbar y módulo de ferias con estilos globales

---
+++ src/pages/Ferias.tsx
@@ -0,0 +1,11 @@
+const Ferias = () => {
+    return (
+        <div className="page-content main-container">
+          <h1>Gestión de Ferias</h1>
+          <p>Aquí podrás visualizar, agregar o modificar información sobre ferias comunitarias.</p>
+        </div>
+      );      
+  };
+  
+  export default Ferias;
+  
