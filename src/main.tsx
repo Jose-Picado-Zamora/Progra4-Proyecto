97bae91e1cab4ae727bbd2334b3ac6601872ab40
RobertoUNA <roberto.campos.calvo@est.una.ac.cr>
2025-05-16 02:24:57 -0600

    borrado de login and authcontext no implementado

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { AuthProvider } from './Context/AuthContext.tsx';
 import { RouterProvider } from '@tanstack/react-router';
 import router from './routes.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
-
-  <AuthProvider>
-    <RouterProvider router= {router}/>
-  </AuthProvider>
-  
+  <RouterProvider router= {router}/>
 );
